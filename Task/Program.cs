746987494d8a37ed812c218bf43774e1f9457c59
Kirill <frantic.87@mail.ru>
2023-03-26 12:45:51 +0300

    added PrintArr3Chars method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] ReadArrStr(string text)
 {
     Console.Write(text);
-    return Console.ReadLine().Split(' ');
+    return Console.ReadLine().Split(", ");
 }
 
 void PrintArr(string[] array)
@@ -9,5 +9,19 @@ void PrintArr(string[] array)
     Console.WriteLine("[" + string.Join(", ", array) + "]");
 }
 
-string[] str = ReadArrStr("Введите массив строк: ");
+void PrintArr3Chars(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+string[] str = ReadArrStr("Введите массив строк через запятую: ");
 PrintArr(str);
+PrintArr3Chars(str);
